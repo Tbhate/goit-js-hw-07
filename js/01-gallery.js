b0e831599f48bc615427783bf249ff4e57bb63ae
Tbhate <dmitriybut83@gmail.com>
2023-06-23 10:32:53 +0300

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+const pictureLink = document.querySelector(".gallery__link");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"></img></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+gallery.addEventListener("click", openOriginal);
+
+function openOriginal({ target }) {
+  event.preventDefault();
+
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${target.dataset.source}"  width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscDown);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onEscDown);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscDown(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// pushing images
+
+const gallery = document.querySelector(".gallery");
+const pictureLink = document.querySelector(".gallery__link");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></img></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+// lightbox
+
+let modalGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
